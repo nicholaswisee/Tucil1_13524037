980e75e66f005d927450ca1545694205ac962ad0
Nicholas Wise <nicholasaragih@gmail.com>
2026-02-12 23:23:55 +0700

    add class for board and positions

--- src/main/java/queens/core/Position.java
+++ src/main/java/queens/core/Position.java
@@ -1,5 +1,60 @@
-public
 package queens.core;
 
-record Position() {
+import java.util.Objects;
+
+public class Position {
+  private final int row;
+  private final int col;
+
+  public Position(int row, int col) {
+    this.row = row;
+    this.col = col;
+  }
+
+  public int getRow() {
+    return row;
+  }
+
+  public int getCol() {
+    return col;
+  }
+
+  // Checks if position is on same row, col, diagonal, or color
+  public boolean attacks(Position other) {
+    if (this.equals(other)) {
+      return false;
+    }
+
+    // Same column
+    if (this.col == other.col) {
+      return true;
+    }
+
+    // Same diagonal (absolute difference of rows equals absolute difference columns)
+    if (Math.abs(this.row - other.row) == Math.abs(this.col - other.col)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    Position position = (Position) o;
+    return row == position.row && col == position.col;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(row, col);
+  }
+
+  @Override
+  public String toString() {
+    return "(" + row + "," + col + ")";
+  }
 }
